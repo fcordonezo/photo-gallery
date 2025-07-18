1a0b9553a8717d5efb8a29a3246dba0678f7b266
fcordonezo <fabiancamilo960720@gmail.com>
2025-07-18 18:26:30 -0500

    feat(script): Added styles for buttons in carousel

--- script.js
+++ script.js
@@ -1,21 +1,22 @@
-import {createPhotoBook} from "./modules/photobook.js";
+import { createPhotoBook } from "./modules/photobook.js";
 
 document.addEventListener("DOMContentLoaded", () => {
 
   createPhotoBook(document.getElementById("photobook-container"));
-  
+
   Fancybox.bind('[data-fancybox="gallery"]', {
+    Hash: false,
+    mainStyle: {
+      "--f-toolbar-padding": "16px 32px",
+      "--f-toolbar-gap": "8px",
+      "--f-button-border-radius": "50%",
+      "--f-thumb-width": "82px",
+      "--f-thumb-height": "82px",
+      "--f-thumb-opacity": "0.5",
+      "--f-thumb-hover-opacity": "1",
+      "--f-thumb-selected-opacity": "1",
+    },
+    showClass: "f-fadeIn",
     contentClick: "iterateZoom",
-    Images: {
-      Panzoom: {
-        maxScale: 1.5,
-      },
-    },
-    Toolbar: {
-      display: {
-        left: ["infobar"],
-        right: ["slideshow", "thumbs", "download", "fullscreen", "share", "close"],
-      },
-    },
   });
 });
