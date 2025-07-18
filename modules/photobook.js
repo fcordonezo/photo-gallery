18229945aa3582a2709c8a9af14015e0ee180501
fcordonezo <fabiancamilo960720@gmail.com>
2025-07-18 18:31:03 -0500

    fix: Changed img to new Image object

--- modules/photobook.js
+++ modules/photobook.js
@@ -1,15 +1,14 @@
 import { photos } from "./photos.js";
 
 export const createPhotoBook = (container) => {
-    photos
-      .forEach((photo) => buildElements(container, photo));
+    photos.forEach((photo) => buildElements(container, photo));
   }
   
   const buildElements = (container, photo) => {
     const card = document.createElement("div");
     const cardImage = document.createElement("div");
     const a = document.createElement("a");
-    const img = document.createElement("img");
+    const img = new Image();
     card.classList.add("card");
     cardImage.classList.add("card-image");
     a.setAttribute("data-fancybox", "gallery");
@@ -17,7 +16,7 @@ export const createPhotoBook = (container) => {
     a.setAttribute("data-slug", photo.name.concat(" en ", photo.place).replaceAll(" ", "-").replaceAll(/[\,\.]*/g,"").toLowerCase());
     a.href = photo.src;
     img.src = photo.thumb;
-    img.alt = photo.name + ". " + photo.place + ". " +  photo.city + (photo.event? ". " + photo.event:"") + ". " + photo.country;
+    img.alt = `${photo.name}. ${photo.place}. ${photo.city}${(photo.event? ". " + photo.event:"")}. ${photo.country}`;
     img.loading = "lazy";
     a.appendChild(img);
     cardImage.appendChild(a);
